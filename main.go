af825e859540e6b13d7e8fdfde57ce8ef78059f0
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-16 20:14:47 -0500

    add main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"f1-go-api/configs"
+	"f1-go-api/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	//fiber instance
+	app := fiber.New()
+
+	//run database
+	configs.ConnectDB()
+
+	//routes
+	routes.UserRoute(app)
+
+	//port
+	app.Listen(":6000")
+}
