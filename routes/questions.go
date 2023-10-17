ceb49f5cc04b4fcb99595e5e276c812d34a757e5
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-16 21:22:43 -0500

    use questions controller

--- routes/questions.go
+++ routes/questions.go
@@ -1,7 +1,11 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"f1-go-api/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func UserRoute(app *fiber.App) {
-
+	app.Get("/questions", controllers.GetQuestions)
 }
