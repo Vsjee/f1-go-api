9ecdd6442e36bd68ce0e7b17bbda2b92613c44dc
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-16 21:22:14 -0500

    update question struct naming

--- models/questions_model.go
+++ models/questions_model.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Questions struct {
-	_id      primitive.ObjectID `json:"id,omitempty"`
-	question string             `json:"question,omitempty" validate:"required"`
-	answers  []string           `json:"answers,omitempty" validate:"required"`
+	Id       primitive.ObjectID `json:"id,omitempty"`
+	Question string             `json:"question,omitempty" validate:"required"`
+	Answers  []string           `json:"answers,omitempty" validate:"required"`
 }
