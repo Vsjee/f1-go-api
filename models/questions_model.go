8f1729d9a3ac0531ff0210604ec06ae341dab650
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-16 20:58:46 -0500

    add fiber types

--- models/questions_model.go
+++ models/questions_model.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Questions struct {
-	_id      primitive.ObjectID
-	question string
-	answers  []string
+	_id      primitive.ObjectID `json:"id,omitempty"`
+	question string             `json:"question,omitempty" validate:"required"`
+	answers  []string           `json:"answers,omitempty" validate:"required"`
 }
--- responses/questions_response.go
+++ responses/questions_response.go
@@ -3,7 +3,7 @@ package responses
 import "github.com/gofiber/fiber/v2"
 
 type QuestionResponse struct {
-	Status  int
-	Message string
-	Data    *fiber.Map
+	Status  int        `json:"status"`
+	Message string     `json:"message"`
+	Data    *fiber.Map `json:"data"`
 }
