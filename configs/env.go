315b767842b500fb4ba23444679d68b1a5dbde27
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-17 21:44:52 -0500

    be better than me twice

--- configs/env.go
+++ configs/env.go
@@ -1,17 +1,5 @@
 package configs
 
-import (
-	"log"
-
-	"github.com/joho/godotenv"
-)
-
 func EnvMongoURI() string {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	return "mongodb+srv://davidfelipehernandez1:rvVAz2hgfAFTSVt1@dbm.h04vkul.mongodb.net/"
 }
